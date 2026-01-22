2bae6f327f0ec059000e44d46a83031993f80236
THE-KEGARE <shigetoraspizza@gmail.com>
2026-01-22 23:45:03 +0300

    Add TrustProxies middleware for correct IP detection

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * trust all proxies because we're behind nginx and life is short
+     */
+    protected $proxies = '*';
+
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
