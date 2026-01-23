6a82dd50d82ec4a39e953a82be47dad849f2af90
THE-KEGARE <shigetoraspizza@gmail.com>
2026-01-23 12:14:41 +0100

    Restrict CORS to specific methods and headers

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     // production: MUST set CORS_ALLOWED_ORIGINS in .env
     // local: defaults to localhost for convenience
@@ -28,11 +28,11 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'X-XSRF-TOKEN'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 3600,
 
     'supports_credentials' => true,
 
